503d5a91f9f7bb821f9642514f1338fcaa8d0c58
Petya29 <petya29avramenko@gmail.com>
2023-05-24 18:01:44 +0200

    Added wrapper shape to cloth

--- src/components/partials/Cloth.tsx
+++ src/components/partials/Cloth.tsx
@@ -7,7 +7,7 @@ export const Cloth = () => {
   const { project, isFetched, isLoading } = useAppSelector(state => state.project);
 
   return (
-    <div>
+    <div className="w-full h-[70%]">
       {isFetched &&
         <div className="my-4 flex flex-col items-center md:block md:mx-10">
           <div>Name: {project.name}</div>
@@ -19,12 +19,18 @@ export const Cloth = () => {
           />
         </div>
       }
-      <div className="flex justify-center">
-        {!isFetched && isLoading
-          ?
+      <div className="flex justify-center w-full h-full">
+        {!isFetched && isLoading &&
           <Spinner />
-          :
-          ""
+        }
+        {isFetched && !isLoading &&
+          <div className="flex flex-col items-stretch w-full h-full">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="cloth-wrapper" width="100%" height="100%" preserveAspectRatio="xMidYMid meet">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox={`0 0 ${project.width} ${project.height}`}>
+                <rect fill="#efefef" width="100%" height="100%"></rect>
+              </svg>
+            </svg>
+          </div>
         }
       </div>
     </div>
